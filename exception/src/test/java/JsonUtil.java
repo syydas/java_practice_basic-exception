29ad982bd059ca5fb444209484bff6a3dbe3754e
Asuu <suqinzheng_0624@163.com>
2020-04-05 14:37:22 +0800

    [Asu] feat: finish the needs

--- exception/src/test/java/JsonUtil.java
+++ exception/src/test/java/JsonUtil.java
@@ -1,15 +1,23 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class JsonUtil {
 
-  private JsonUtil() {
-  }
-  private static final ObjectMapper objectMapper = new ObjectMapper();
+    private JsonUtil() {
+    }
 
-  public static String convertToJson(Object object) {
-    //TODO: change the code to pass the test
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    throw new NotImplementedException();
-  }
+    public static String convertToJson(Object object) {
+        //TODO: change the code to pass the test
+
+        String json = null;
+        try {
+            json = objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
 }
